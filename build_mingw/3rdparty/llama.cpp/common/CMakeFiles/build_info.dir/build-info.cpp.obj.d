db630041f957e783ebee88694175b88ff7316217
frasod <frasod@yahoo.com>
2025-10-28 23:35:22 -0400

    Complete migration to i2_s format for VA offline deployment - Models: Microsoft BitNet-b1.58-2B-4T (1.19 GB i2_s GGUF) - Documentation: Updated README, new DEPLOYMENT.md, detailed DEVLOG - Configuration: Git LFS for model distribution (no HuggingFace dependency) - Removed: Build artifacts, development tools, old models

--- build_mingw/3rdparty/llama.cpp/common/CMakeFiles/build_info.dir/build-info.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-3rdparty/llama.cpp/common/CMakeFiles/build_info.dir/build-info.cpp.obj: \
- C:\Users\VHAWRJDRESCF\OneDrive\ -\ Department\ of\ Veterans\ Affairs\Documents\GitHub\VAbitnet\3rdparty\llama.cpp\common\build-info.cpp
--- build_mingw/3rdparty/llama.cpp/examples/gguf-hash/CMakeFiles/xxhash.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/xxhash.dir/deps/xxhash/xxhash.c.obj"
-  "CMakeFiles/xxhash.dir/deps/xxhash/xxhash.c.obj.d"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/xxhash.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
